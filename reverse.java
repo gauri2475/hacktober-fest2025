3ba209a2925ad6acad668022a4a12ac8faf17757
gauri2475 <gaurimalik24@gmail.com>
2025-10-23 22:47:25 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,20 @@
+// Java program to reverse a string using backward traversal
+
+class GfG {
+    static String reverseString(String s) {
+        StringBuilder res = new StringBuilder();
+  
+        // Traverse on s in backward direction
+        // and add each character to a new string
+        for (int i = s.length() - 1; i >= 0; i--) {
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "abdcfe";
+        String res = reverseString(s);
+        System.out.print(res);
+    }
+}
